f37a18188584af9092e8e6d99f21a46cd74def8b
Mr-Gonza <55816431+Mr-Gonza@users.noreply.github.com>
2019-09-25 20:41:28 -0300

    Add files via upload

---
+++ Mat.py
@@ -0,0 +1,10 @@
+
+def mat_status(x):
+    if x == "yes":
+        return "Mat es un PELOTUDO"
+    else:
+        return "Invalid input, try \"yes\""
+
+print(mat_status(input("Es Mat un  pelotudo?\n")))
+
+
---
+++ Mat_2.0.py
@@ -0,0 +1,7 @@
+while True:
+    x = input("Es mat un pelotudo?\n")
+    if x == "yes":
+        print("\nMat es un PELOTUDO")
+        break
+    else:
+        print("\nInvalid input, try \"yes\"\n")
